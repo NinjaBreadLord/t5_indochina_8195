ee20f7f524c16ed8126a937a6a650007cb0bf33e
NinjaBreadLord <everittcheng@gmail.com>
2022-12-13 22:41:32 -0800

    123123

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/Calculator.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/Calculator.java
@@ -1,8 +1,10 @@
 package com.nighthawk.spring_portfolio.mvc.calculator;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
+
 /* In mathematics,
     an expression or mathematical expression is a finite combination of symbols that is well-formed
     according to rules that depend on the context.
@@ -37,9 +39,11 @@ public class Calculator {
         SEPARATORS.put("(", 0);
         SEPARATORS.put(")", 0);
     }
+
     // Create a 1 argument constructor expecting a mathematical expression
     public Calculator(String expression) {
-        // original input
+        this.parenthesesCheck();
+        // original inputreverse_polish
         this.expression = expression;
         // parse expression into terms
         this.termTokenizer();
@@ -48,51 +52,53 @@ public class Calculator {
         // calculate reverse polish notation
         this.rpnToResult();
     }
+
     // Test if token is an operator
     private boolean isOperator(String token) {
         // find the token in the hash map
         return OPERATORS.containsKey(token);
     }
+
     // Test if token is an separator
     private boolean isSeparator(String token) {
         // find the token in the hash map
         return SEPARATORS.containsKey(token);
     }
-    public static boolean balancedParenthensies(ArrayList<String> s) {
-        int open = 0;
-        int close = 0;
-        for (String token : s) {
-            if (token == "(") {
-                open++;
-            } else if (token == ")") {
-                close++;
-            }
-            if (open < close) {
-                return false;
+
+    private boolean parenthesesCheck() {
+        int leftParentheses = 0;
+        int rightParentheses = 0;
+        for (int i = 0; i < this.expression.length(); i++) {
+            if (this.expression.charAt(i) == '(') {
+                leftParentheses++;
+            } else if (this.expression.charAt(i) == ')') {
+                rightParentheses++;
             }
         }
-        if (open == close) {
+
+        if (leftParentheses != rightParentheses) {
             return true;
-        } 
-        
-        else {
+        } else {
             return false;
         }
     }
+
     // Compare precedence of operators.
     private Boolean isPrecedent(String token1, String token2) {
         // token 1 is precedent if it is greater than token 2
-        return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0) ;
+        return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0);
     }
-    // Term Tokenizer takes original expression and converts it to ArrayList of tokens
+
+    // Term Tokenizer takes original expression and converts it to ArrayList of
+    // tokens
     private void termTokenizer() {
         // contains final list of tokens
         this.tokens = new ArrayList<>();
-        int start = 0;  // term split starting index
-        StringBuilder multiCharTerm = new StringBuilder();    // term holder
+        int start = 0; // term split starting index
+        StringBuilder multiCharTerm = new StringBuilder(); // term holder
         for (int i = 0; i < this.expression.length(); i++) {
             Character c = this.expression.charAt(i);
-            if ( isOperator(c.toString() ) || isSeparator(c.toString())  ) {
+            if (isOperator(c.toString()) || isSeparator(c.toString())) {
                 // 1st check for working term and add if it exists
                 if (multiCharTerm.length() > 0) {
                     tokens.add(this.expression.substring(start, i));
@@ -115,8 +121,10 @@ public class Calculator {
             tokens.add(this.expression.substring(start));
         }
     }
-    // Takes tokens and converts to Reverse Polish Notation (RPN), this is one where the operator follows its operands.
-    private void tokensToReversePolishNotation () {
+
+    // Takes tokens and converts to Reverse Polish Notation (RPN), this is one where
+    // the operator follows its operands.
+    private void tokensToReversePolishNotation() {
         // contains final list of tokens in RPN
         this.reverse_polish = new ArrayList<>();
         // stack is used to reorder for appropriate grouping and precedence
@@ -128,9 +136,8 @@ public class Calculator {
                     tokenStack.push(token);
                     break;
                 case ")":
-                    while (tokenStack.peek() != null && !tokenStack.peek().equals("("))
-                    {
-                        reverse_polish.add( tokenStack.pop() );
+                    while (tokenStack.peek() != null && !tokenStack.peek().equals("(")) {
+                        reverse_polish.add(tokenStack.pop());
                     }
                     tokenStack.pop();
                     break;
@@ -143,9 +150,8 @@ public class Calculator {
                     // While stack
                     // not empty AND stack top element
                     // and is an operator
-                    while (tokenStack.size() > 0 && isOperator(tokenStack.peek()))
-                    {
-                        if ( isPrecedent(token, tokenStack.peek() )) {
+                    while (tokenStack.size() > 0 && isOperator(tokenStack.peek())) {
+                        if (isPrecedent(token, tokenStack.peek())) {
                             reverse_polish.add(tokenStack.pop());
                             continue;
                         }
@@ -154,7 +160,7 @@ public class Calculator {
                     // Push the new operator on the stack
                     tokenStack.push(token);
                     break;
-                default:    // Default should be a number, there could be test here
+                default: // Default should be a number, there could be test here
                     this.reverse_polish.add(token);
             }
         }
@@ -163,66 +169,62 @@ public class Calculator {
             reverse_polish.add(tokenStack.pop());
         }
     }
+
     // Takes RPN and produces a final result
-    private void rpnToResult()
-    {
+    private void rpnToResult() {
         // stack is used to hold operands and each calculation
         Stack<Double> calcStack = new Stack<Double>();
         // RPN is processed, ultimately calcStack has final result
-        if(balancedParenthensies(this.tokens)){
-            for (String token : this.reverse_polish){
-                // If the token is an operator, calculate
-                if (isOperator(token))
-                {
-                    // Pop the two top entries
-                    Double ent1 = Double.valueOf(calcStack.pop());
-                    Double ent2 = Double.valueOf(calcStack.pop());
-                    // Calculate intermediate results
-                    result = 0.0;
-                    switch(token) {
-                        case "+":
-                            result = ent1 + ent2;
-                            break;
-                        case "-":
-                            result = ent2 - ent1;
-                            break;
-                        case "*":
-                            result = ent1 * ent2;
-                            break;
-                        case "除":
-                            result = ent2 / ent1;
-                            break;
-                        case "％":
-                            result = ent2 % ent1;
-                            break;
-                        case "^":
-                            result = Math.pow(ent2, ent1);
-                            break;
-                        default:
-                            result = null;
-                            break;
-                    }
-                    // Push intermediate result back onto the stack
-                    calcStack.push( result );
+        for (String token : this.reverse_polish) {
+            // If the token is an operator, calculate
+            if (isOperator(token)) {
+                // Pop the two top entries
+                Double ent1 = Double.valueOf(calcStack.pop());
+                Double ent2 = Double.valueOf(calcStack.pop());
+                // Calculate intermediate results
+                result = 0.0;
+                switch (token) {
+                    case "+":
+                        result = ent1 + ent2;
+                        break;
+                    case "-":
+                        result = ent2 - ent1;
+                        break;
+                    case "*":
+                        result = ent1 * ent2;
+                        break;
+                    case "除":
+                        result = ent2 / ent1;
+                        break;
+                    case "％":
+                        result = ent2 % ent1;
+                        break;
+                    case "^":
+                        result = Math.pow(ent2, ent1);
+                        break;
+                    default:
+                        result = null;
+                        break;
                 }
-                // else the token is a number push it onto the stack
-                else{
-                    calcStack.push(Double.valueOf(token));
-                }
-                }
-                this.result = calcStack.pop();
-            }else {
-                System.out.println("Unbalanced");
+                // Push intermediate result back onto the stack
+                calcStack.push(result);
             }
+            // else the token is a number push it ont the stack
+            else
+                calcStack.push(Double.valueOf(token));
+        }
+        this.result = calcStack.pop();
+
     }
-   
+
     // Print the expression, terms, and result
     public String toString() {
         return ("Original expression: " + this.expression + "\n" +
                 "Tokenized expression: " + this.tokens.toString() + "\n" +
-                "Reverse Polish Notation: " +this.reverse_polish.toString() + "\n" +
+                "Reverse Polish Notation: " + this.reverse_polish.toString() + "\n" +
                 "Final result: " + String.format("%.2f", this.result));
     }
+
     // Tester method
     public static void main(String[] args) {
         // Random set of test cases
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard/LightBoard.java
@@ -0,0 +1,240 @@
+package com.nighthawk.spring_portfolio.mvc.lightboard;
+import java.util.Scanner;
+import lombok.Data;
+
+@Data  // Annotations to simplify writing code (ie constructors, setters)
+public class LightBoard {
+    private Light[][] lights;
+
+    /* Initialize LightBoard and Lights */
+    public LightBoard(int numRows, int numCols) {
+        this.lights = new Light[numRows][numCols];
+        // 2D array nested loops, used for initialization
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                lights[row][col] = new Light();  // each cell needs to be constructed
+            }
+        }
+
+    }
+
+    /* Output is intended for API key/values */
+    public String toString() { 
+        String outString = "[";
+        // 2D array nested loops, used for reference
+        for (int row = 0; row < lights.length; row++) {
+            for (int col = 0; col < lights[row].length; col++) {
+                outString += 
+                // data
+                "{" + 
+                "\"row\": " + row + "," +
+                "\"column\": " + col + "," +
+                "\"light\": " + lights[row][col] +   // extract toString data
+                "}," ;
+            }
+        }
+        // remove last comma, newline, add square bracket, reset color
+        outString = outString.substring(0,outString.length() - 1) + "]";
+		return outString;
+    }
+
+    /* Output is intended for Terminal, effects added to output */
+    public String toTerminal() { 
+        String outString = "[";
+        // 2D array nested loops, used for reference
+        for (int row = 0; row < lights.length; row++) {
+            for (int col = 0; col < lights[row].length; col++) {
+                outString += 
+                // reset
+                "\033[m" +
+                
+                // color
+                "\033[38;2;" + 
+                lights[row][col].getRed() + ";" +  // set color using getters
+                lights[row][col].getGreen() + ";" +
+                lights[row][col].getBlue() + ";" +
+                lights[row][col].getEffect() + "m" +
+                // data, extract custom getters
+                "{" +
+                "\"" + "isOn\": " + lights[row][col].isOn() +
+                "," +
+                "\"" + "RGB\": " + "\"" + lights[row][col].getRGB() + "\"" +
+                "," +
+                "\"" + "Effect\": " + "\"" + lights[row][col].getEffectTitle() + "\"" +
+                "}," +
+                // newline
+                "\n" ;
+            }
+        }
+        // remove last comma, newline, add square bracket, reset color
+        outString = outString.substring(0,outString.length() - 2) + "\033[m" + "]";
+		return outString;
+    }
+
+    /* Output is intended for Terminal, draws color palette */
+    public String toColorPalette(int bHeight, int bLength) {
+        // block sizes
+        final int ROWS = bHeight;
+        final int COLS = bLength;
+
+        // Build large string for entire color palette
+        String outString = "";
+        // find each row
+        for (int row = 0; row < lights.length; row++) {
+            // repeat each row for block size
+            for (int i = 0; i < ROWS; i++) {
+                // find each column
+                for (int col = 0; col < lights[row].length; col++) {
+                    // repeat each column for block size
+                    for (int j = 0; j < COLS; j++) {
+                        // print single character, except at midpoint print color code
+                        if (lights[row][col].isOn()) {
+                            String c = (i == (int) (ROWS / 2) && j == (int) (COLS / 2) ) 
+                            ? lights[row][col].getRGB()
+                            : (j == (int) (COLS / 2))  // nested ternary
+                            ? " ".repeat(lights[row][col].getRGB().length())
+                            : " ";
+
+                        outString += 
+                        // reset
+                        "\033[m" +
+                        
+                        // color
+                        "\033[38;2;" + 
+                        lights[row][col].getRed() + ";" +
+                        lights[row][col].getGreen() + ";" +
+                        lights[row][col].getBlue() + ";" +
+                        "7m" +
+
+                        // color code or blank character
+                        c +
+
+                        // reset
+                        "\033[m";
+                        }
+                        
+                    }
+                }
+                outString += "\n";
+            }
+        }
+        // remove last comma, newline, add square bracket, reset color
+        outString += "\033[m";
+		return outString;
+    }
+    
+    public void flag(String flag) {
+        if(flag == "Belgium") {
+            short rv = 45;
+            short gv = 41;
+            short bv = 38;
+
+            for (int rowz = 0; rowz < 5; rowz++) {
+                for (int colz = 0; colz < 3; colz++) {
+                    lights[rowz][colz].RGBset(rv,gv,bv);
+                }
+            } 
+            short rva = 255;
+            short gva = 205;
+            short bva = 0;
+
+            for (int rowz = 0; rowz < 5; rowz++) {
+                for (int colz = 3; colz < 6; colz++) {
+                    lights[rowz][colz].RGBset(rva,gva,bva);
+                }
+            } 
+            short rvb = 200;
+            short gvb = 16;
+            short bvb = 46;
+
+            for (int rowz = 0; rowz < 5; rowz++) {
+                for (int colz = 6; colz < 9; colz++) {
+                    lights[rowz][colz].RGBset(rvb,gvb,bvb);
+                }
+            } 
+        }
+
+        if(flag == "Russia") {
+            short rv = 255;
+            short gv = 255;
+            short bv = 255;
+
+            for (int rowz = 0; rowz < 2; rowz++) {
+                for (int colz = 0; colz < 9; colz++) {
+                    lights[rowz][colz].RGBset(rv,gv,bv);
+                }
+            } 
+            short ru = 28;
+            short gu = 53;
+            short bu = 120;
+            for (int rowz = 2; rowz < 4; rowz++) {
+                for (int colz = 0; colz < 9; colz++) {
+                    lights[rowz][colz].RGBset(ru,gu,bu);
+                }
+            } 
+            short rr = 255;
+            short gr = 0;
+            short br = 0;
+            for (int rowz = 4; rowz < 5; rowz++) {
+                for (int colz = 0; colz < 9; colz++) {
+                    lights[rowz][colz].RGBset(rr,gr,br);
+                }
+            }
+        }
+
+        if(flag == "Japan") {
+            short rx = 255;
+            short gx = 255;
+            short bx = 255;
+
+            for (int rowz = 0; rowz < 5; rowz++) {
+                for (int colz = 0; colz < 9; colz++) {
+                    lights[rowz][colz].RGBset(rx,gx,bx);
+                }
+            } 
+            short r = 255;
+            short g = 0;
+            short b = 0;
+            for (int rowz = 1; rowz < 4; rowz++) {
+                for (int colz = 3; colz < 6; colz++) {
+                    lights[rowz][colz].RGBset(r,g,b);
+                }
+            }
+        } 
+
+    }
+
+    public void turnOff(int zrow, int zcol) {
+        lights[zrow][zcol].setOn(false);
+    }
+
+    static public void main(String[] args) {
+        // create and display LightBoard
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Rows");
+        int bRows = scan.nextInt();
+        System.out.println("Columns");
+        int bCols = scan.nextInt();
+        System.out.println("Height");
+        int bHeight = scan.nextInt();
+        System.out.println("Length");
+        int bLength = scan.nextInt();
+        LightBoard lightBoard = new LightBoard(bRows, bCols);
+        
+        System.out.println(lightBoard);  // use toString() method
+        System.out.println(lightBoard.toTerminal());
+        System.out.println(lightBoard.toColorPalette(bHeight, bLength));
+        lightBoard.turnOff(0, 0);
+        System.out.println(lightBoard.toTerminal());
+        System.out.println(lightBoard.toColorPalette(bHeight, bLength));
+        
+        LightBoard flagBoard = new LightBoard(5, 9);
+        flagBoard.flag("Germany");
+        System.out.println(flagBoard.toColorPalette(4, 4));
+        flagBoard.flag("Russia");
+        System.out.println(flagBoard.toColorPalette(4, 4));
+        flagBoard.flag("Japan");
+        System.out.println(flagBoard.toColorPalette(4, 4));
+
+    }
+}
