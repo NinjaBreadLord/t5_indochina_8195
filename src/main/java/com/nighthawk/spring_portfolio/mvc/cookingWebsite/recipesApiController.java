d52a6a939a47142907a94dc1687039ab07d6f76d
NinjaBreadLord <everittcheng@gmail.com>
2023-02-06 00:43:18 -0800

    123

--- src/main/java/com/nighthawk/spring_portfolio/mvc/cookingWebsite/recipesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/cookingWebsite/recipesApiController.java
@@ -18,41 +18,41 @@ import java.text.SimpleDateFormat;
 @RequestMapping("/api/recipes/")
 public class recipesApiController {
 
-    @Autowired 
+    @Autowired
     recipesJPARepository repository;
 
     @GetMapping("all")
     public ResponseEntity<List<Recipes>> getAllRecipes() {
-        return new ResponseEntity<>( repository.findAllByOrderByNameAsc(), HttpStatus.OK);
+        return new ResponseEntity<>(repository.findAllByOrderByNameAsc(), HttpStatus.OK);
     }
 
     /*
-    GET individual Car using ID
+     * GET individual Car using ID
      */
     @GetMapping("{id}")
     public ResponseEntity<Recipes> getRecipes(@PathVariable long id) {
-        return new ResponseEntity<>( repository.getRecipe(id), HttpStatus.OK);
+        return new ResponseEntity<>(repository.getRecipe(id), HttpStatus.OK);
     }
 
     /*
-    DELETE individual Car using ID
+     * DELETE individual Car using ID
      */
     @DeleteMapping("delete/{id}")
     public ResponseEntity<Object> deleteRecipes(@PathVariable long id) {
         repository.deleteRecipe(id);
-        return new ResponseEntity<>( ""+ id +" deleted", HttpStatus.OK);
+        return new ResponseEntity<>("" + id + " deleted", HttpStatus.OK);
     }
 
     /*
-    POST Aa record by Requesting Parameters from URI
+     * POST Aa record by Requesting Parameters from URI
      */
-    @PostMapping( "/post/")
-    public ResponseEntity<Object> postPerson(@RequestParam("name") String name, @RequestParam("ingredients") String ingredients,
-                                             @RequestParam("description") String description) {
+    @PostMapping("/post/")
+    public ResponseEntity<Object> postPerson(@RequestParam("name") String name,
+            @RequestParam("ingredients") String ingredients,
+            @RequestParam("description") String description) {
 
         repository.saveRecipe(new Recipe(null, name, ingredients, description));
-        return new ResponseEntity<>(name +" is created successfully", HttpStatus.CREATED);
+        return new ResponseEntity<>(name + " is created successfully", HttpStatus.CREATED);
     }
-
 
 }
