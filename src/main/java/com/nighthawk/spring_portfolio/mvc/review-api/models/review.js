ae290064d6ace7499e4cf80f3767ecc71f3f26bf
Ritvik <ritvik@gmail.com>
2023-02-09 18:37:45 -0800

    asd

--- src/main/java/com/nighthawk/spring_portfolio/mvc/review-api/models/review.js
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/review-api/models/review.js
@@ -1,14 +1,14 @@
-class Review {
-    constructor(text, author, recipe, date, rating) {
-      this.text = text;
-      this.author = author;
-      this.recipe = recipe;
-      this.date = date;
-      this.rating = rating;
-    }
-  }
+//class Review {
+ //   constructor(text, author, recipe, date, rating) {
+ //     this.text = text;
+//      this.author = author;
+ //     this.recipe = recipe;
+ //     this.date = date;
+ //     this.rating = rating;
+  //  }
+  //}
   
-  //Plan on converting this portion into a database (.db file with SQL or MongoDB)
-  let reviews = [
-    new Review('The food was elite eats', 'Ritvik', 'Mac and Cheese', '2022-07-1975', 5)
-  ];
+//  //Plan on converting this portion into a database (.db file with SQL or MongoDB)
+ //// let reviews = [
+ //   new Review('The food was elite eats', 'Ritvik', 'Mac and Cheese', '2022-07-1975', 5)
+ // ];
--- src/main/java/com/nighthawk/spring_portfolio/mvc/review-api/server.js
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/review-api/server.js
@@ -1,55 +1,55 @@
-const express = require('express');
-const app = express();
-const port = 8000;
+//const express = require('express');
+//const app = express();
+//const port = 8000;
 
 // GET /reviews
-app.get('/reviews', (req, res) => {
-  res.send('Get all reviews');
-});
+////app.get('/reviews', (req, res) => {
+//  res.send('Get all reviews');
+//});
 
 // GET /reviews/:id
-app.get('/reviews/:id', (req, res) => {
-  res.send(`Get review with id: ${req.params.id}`);
-});
+//app.get('/reviews/:id', (req, res) => {
+//  res.send(`Get review with id: ${req.params.id}`);
+//});
 
 // POST /reviews
-app.post('/reviews', (req, res) => {
-  res.send('Create a new review');
-});
+//app.post('/reviews', (req, res) => {
+//  res.send('Create a new review');
+//});
 
 // PUT /reviews/:id
-app.put('/reviews/:id', (req, res) => {
-  res.send(`Update review with id: ${req.params.id}`);
-});
+//app.put('/reviews/:id', (req, res) => {
+//  res.send(`Update review with id: ${req.params.id}`);
+//});
 
 // DELETE /reviews/:id
-app.delete('/reviews/:id', (req, res) => {
-  res.send(`Delete review with id: ${req.params.id}`);
-});
+//app.delete('/reviews/:id', (req, res) => {
+//  res.send(`Delete review with id: ${req.params.id}`);
+//});
 
-app.listen(port, () => {
-  console.log(`Reviews API running at http://localhost:${port}`);
-});
+//app.listen(port, () => {
+//  console.log(`Reviews API running at http://localhost:${port}`);
+//});
 
-const express = require('express');
-const Review = require('./models/review');
-const app = express();
-const port = 3000;
+//const express = require('express');
+////const Review = require('./models/review');
+//const app = express();
+//const port = 3000;
 
 // GET /reviews
-app.get('/reviews', (req, res) => {
-  res.json(Review.all());
-});
+//app.get('/reviews', (req, res) => {
+//  res.json(Review.all());
+//});
 
 // GET /reviews/:id
-app.get('/reviews/:id', (req, res) => {
-  const review = Review.find(req.params.id);
-  if (!review) return res.status(404).send('Review not found');
-  res.json(review);
-});
+//app.get('/reviews/:id', (req, res) => {
+ // const review = Review.find(req.params.id);
+//  if (!review) return res.status(404).send('Review not found');
+//  res.json(review);
+//});
 
 // POST /reviews
-app.post('/reviews', (req, res) => {
-  const review = Review.create(req.body);
-  res.status(201).json(review);
-});
+//app.post('/reviews', (req, res) => {
+//  const review = Review.create(req.body);
+//  res.status(201).json(review);
+//});
