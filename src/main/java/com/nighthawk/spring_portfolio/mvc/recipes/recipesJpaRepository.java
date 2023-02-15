61210fc60ebd6ca04c244ba07bbe15db8b4d0fed
NinjaBreadLord <everittcheng@gmail.com>
2023-02-15 11:55:58 -0800

    need to get my story straight

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/recipes/recipesJpaRepository.java
@@ -0,0 +1,13 @@
+package com.nighthawk.spring_portfolio.mvc.recipes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+
+public interface recipesJpaRepository extends JpaRepository<recipes, Long> {
+    recipes findByName(String name);
+    List<recipes> findAllByOrderByNameAsc();
+    List<recipes> findAll();
+}
