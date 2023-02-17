f407bb74035a4f02261556521c285d4d3445246a
samuelwaang <samuelwaang@gmail.com>
2023-02-17 10:35:33 -0800

    security change

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.antMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
 				.antMatchers("/api/person/").authenticated()
+				.antMatchers("/api/recipes/**").authenticated()
 				.and()
 			// support cors
 			.cors().and()
